9f8b05d4ee4bce62537a473c34c01a003d7d49ed
Yakubov <119720457+FenixMerlin@users.noreply.github.com>
2024-08-15 19:54:19 +0600

    Delete SunRent directory

--- SunRent/DB/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def index(request):
-  return HttpResponse("Hello, this is the index page of myapp.")
-
-def maps(request):
-  return HttpResponse("This is the about page of myapp.")
